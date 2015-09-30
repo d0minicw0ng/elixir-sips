a86b49c554ce479cc8d3f767596811fdec87849b
Dominic Wong <dominic.wong.617@gmail.com>
2015-09-30 18:29:35 +0800

    023 OTP part 5 supervisors and persistent state

--- supervised_list_server/lib/list_supervisor.ex
+++ supervised_list_server/lib/list_supervisor.ex
@@ -2,11 +2,19 @@ defmodule ListSupervisor do
   use Supervisor
 
   def start_link do
-    :supervisor.start_link(__MODULE__, [])
+    result = {:ok, sup} = :supervisor.start_link(__MODULE__, [])
+    start_workers(sup)
+    result
   end
 
-  def init(list) do
-    child_processes = [ worker(ListServer, list) ]
-    supervise child_processes, strategy: :one_for_one
+  def start_workers(sup) do
+    # start the ListData worker
+    {:ok, list_data} = :supervisor.start_child(sup, worker(ListData, []))
+    # now start the SubSupervisor for the actual ListServer
+    :supervisor.start_child(sup, worker(ListSubSupervisor, [list_data]))
+  end
+
+  def init(_) do
+    supervise [], strategy: :one_for_one
   end
 end
