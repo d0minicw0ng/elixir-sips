4eeef320824a46f5634f259ebf6753123201864b
Dominic Wong <dominic.wong.617@gmail.com>
2015-10-07 17:42:06 +0800

    037 ExLager

--- supervised_list_server/mix.exs
+++ supervised_list_server/mix.exs
@@ -14,7 +14,7 @@ defmodule SupervisedListServer.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:exlager]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule SupervisedListServer.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{ :exlager, github: "khia/exlager" }]
   end
 end
